791f8a6b37484dc6ad8eecbe98db6c56896e28ee
Iris Dregnat <idregnat.mma-m2015@fh-salzburg.ac.at>
2020-03-01 15:10:21 +0100

    Error fix so it will start

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@
 // /client/App.js
 import React, { Component } from 'react';
 import ClothesApi from './ClothesApi';
-import ClothList from './ClothList';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <ClothList />
+                <ClothesApi />
             </div>
         );
     }
